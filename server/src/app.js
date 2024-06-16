f923f16d7c56998886f27dbd7ceda34740614620
sushant696 <sushantprasaii7@gmail.com>
2024-06-16 18:48:20 +0545

    feat: methods avaliablee

--- server/src/app.js
+++ server/src/app.js
@@ -1,33 +1,36 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
+import cors from "cors";
 const app = express();
 
-const allowedOrigins = ['https://to-dos-khaki.vercel.app', 'http://localhost:5173'];
+const allowedOrigins = [
+  "https://to-dos-khaki.vercel.app",
+  "http://localhost:5173",
+];
 
 const corsOptions = {
-    credentials: true,
-    origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+  credentials: true,
+  methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
     }
+  },
 };
 
 app.use(cors(corsOptions));
-app.use(express.json({ limit: '16kb' })) // limiting the incomming json
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-app.use(express.static("public")) // public is folder name
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" })); // limiting the incomming json
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public")); // public is folder name
+app.use(cookieParser());
 
-// router imports 
+// router imports
 import userRouter from "./routes/user.routes.js";
 import TodoRotuer from "./routes/todo.routes.js";
 
-
-// router declarations 
+// router declarations
 app.use("/api/user", userRouter);
 app.use("/api/todo", TodoRotuer);
 
